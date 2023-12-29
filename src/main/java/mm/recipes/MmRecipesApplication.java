7d03e6944a57c52660a36488525ecce8e342ecd6
Mariia Maksimova <mariia.maksimova@transferwise.com>
2023-12-29 18:36:48 +0000

    initial commit
    Signed-off-by: Mariia Maksimova <mariia.maksimova@transferwise.com>

---
+++ src/main/java/mm/recipes/MmRecipesApplication.java
@@ -0,0 +1,13 @@
+package mm.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MmRecipesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mm/recipes/MmRecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package mm.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmRecipesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
