4babbc813617b71ef2ede41d35d90ef2a6c3d12e
Mariia Maksimova <mariia.maksimova@transferwise.com>
2024-01-01 13:10:16 +0000

    Add Ingredient and UnitOfMeasure
    Signed-off-by: Mariia Maksimova <mariia.maksimova@transferwise.com>

--- src/main/java/mm/recipes/domain/Recipe.java
+++ src/main/java/mm/recipes/domain/Recipe.java
@@ -2,6 +2,9 @@ package mm.recipes.domain;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Entity
 public class Recipe {
     @Id
@@ -15,6 +18,9 @@ public class Recipe {
     private String url;
     private String directions;
     // private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients = new HashSet<>();
     @Lob
     private Byte[] image;
     @OneToOne(cascade = CascadeType.ALL)
@@ -98,5 +104,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
     }
 }
---
+++ src/main/java/mm/recipes/domain/UnitOfMeasure.java
@@ -0,0 +1,26 @@
+package mm.recipes.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String unitOfMeasure;
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(String unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+}
