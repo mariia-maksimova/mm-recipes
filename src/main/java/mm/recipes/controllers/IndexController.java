cefea5106aa716441b41125f50ba4fba8681d00b
Mariia Maksimova <mariia.maksimova@transferwise.com>
2024-01-07 17:05:14 +0000

    Spring Data JPA Query Methods
    Signed-off-by: Mariia Maksimova <mariia.maksimova@transferwise.com>

--- src/main/java/mm/recipes/controllers/IndexController.java
+++ src/main/java/mm/recipes/controllers/IndexController.java
@@ -1,14 +1,30 @@
 package mm.recipes.controllers;
 
+import lombok.AllArgsConstructor;
+import mm.recipes.domain.Category;
+import mm.recipes.domain.UnitOfMeasure;
+import mm.recipes.repositories.CategoryRepository;
+import mm.recipes.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
+@AllArgsConstructor
 public class IndexController {
+
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
 
     @RequestMapping({"", "/", "/index", "/index.html"})
     public String getIndexPage() {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Italian");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Category Id is: " + (categoryOptional.isPresent() ? categoryOptional.get().getId() : "Not found"));
+        System.out.println("UOM Id is: " + (unitOfMeasureOptional.isPresent() ? unitOfMeasureOptional.get().getId() : "Not found"));
+
         return "index";
     }
 }
